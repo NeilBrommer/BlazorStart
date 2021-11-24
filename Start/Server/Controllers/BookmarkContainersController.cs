4623d8838d5978a33a71fa94c0ad337ad764ce2c
Neil Brommer <NeilBrommer@users.noreply.github.com>
2021-11-23 21:55:04 -0800

    Move bookmark container API endpoints to their own controller

---
+++ Start/Server/Controllers/BookmarkContainersController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Start.Server.Data.Services.Interfaces;
+using Start.Server.Extensions;
+using Start.Shared;
+
+namespace Start.Server.Controllers {
+	[Authorize]
+	[ApiController]
+	[Route("[controller]")]
+	public class BookmarkContainersController : ControllerBase {
+		private readonly IBookmarkContainerService bookmarkContainerService;
+
+		public BookmarkContainersController(IBookmarkContainerService bookmarkContainerService) {
+			this.bookmarkContainerService = bookmarkContainerService;
+		}
+
+		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status200OK,
+			Type = typeof(IEnumerable<BookmarkContainerDto>))]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public IActionResult GetAllBookmarkContainers() {
+			List<BookmarkContainerDto>? containers = this.bookmarkContainerService
+				.GetUserBookmarkContainers(this.GetAuthorizedUserId())
+				.Select(bc => bc.MapToDto())
+				.ToList();
+
+			if (containers == null)
+				return NotFound();
+
+			return Ok(containers);
+		}
+
+		[HttpGet]
+		[Route("{bookmarkContainerId}")]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BookmarkContainerDto))]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public IActionResult GetBookmarkContainer(int bookmarkContainerId) {
+			BookmarkContainerDto? container = this.bookmarkContainerService
+				.GetBookmarkContainer(this.GetAuthorizedUserId(), bookmarkContainerId, true, true)
+				?.MapToDto();
+
+			if (container == null)
+				return NotFound();
+
+			return Ok(container);
+		}
+
+		[HttpPost]
+		[Route("Create")]
+		[ProducesResponseType(StatusCodes.Status201Created, Type = typeof(BookmarkContainerDto))]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public IActionResult CreateBookmarkContainer([FromBody] string title) {
+			BookmarkContainerDto? container = this.bookmarkContainerService
+				.CreateBookmarkContainer(this.GetAuthorizedUserId(), title)
+				?.MapToDto();
+
+			if (container == null)
+				return BadRequest();
+
+			return Created(
+				Url.Action(nameof(this.GetBookmarkContainer),
+					new { bookmarkContainerId = container.BookmarkContainerId }),
+				container);
+		}
+
+		[HttpDelete]
+		[Route("Delete/{bookmarkContainerId}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public IActionResult DeleteBookmarkContainer(int bookmarkContainerId) {
+			bool res = this.bookmarkContainerService
+				.DeleteBookmarkContainer(this.GetAuthorizedUserId(), bookmarkContainerId);
+
+			if (!res)
+				return NotFound();
+
+			return Ok();
+		}
+	}
+}
--- Start/Server/Controllers/BookmarksController.cs
+++ Start/Server/Controllers/BookmarksController.cs
@@ -12,45 +12,13 @@ namespace Start.Server.Controllers {
 	[ApiController]
 	[Route("[controller]/[action]")]
 	public class BookmarksController : ControllerBase {
-		private readonly IBookmarkContainerService bookmarkContainerService;
 		private readonly IBookmarkGroupService bookmarkGroupService;
 		private readonly IBookmarkService bookmarkService;
 
-		public BookmarksController(IBookmarkContainerService bookmarkContainerService,
-			IBookmarkGroupService bookmarkGroupService, IBookmarkService bookmarkService) {
-			this.bookmarkContainerService = bookmarkContainerService;
+		public BookmarksController(IBookmarkGroupService bookmarkGroupService,
+			IBookmarkService bookmarkService) {
 			this.bookmarkGroupService = bookmarkGroupService;
 			this.bookmarkService = bookmarkService;
-		}
-
-		[HttpGet]
-		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<BookmarkContainerDto>))]
-		[ProducesResponseType(StatusCodes.Status404NotFound)]
-		public IActionResult GetAllBookmarkContainers() {
-			List<BookmarkContainerDto>? containers = this.bookmarkContainerService
-				.GetUserBookmarkContainers(this.GetAuthorizedUserId())
-				.Select(bc => bc.MapToDto())
-				.ToList();
-
-			if (containers == null)
-				return NotFound();
-
-			return Ok(containers);
-		}
-
-		[HttpGet]
-		[Route("{bookmarkContainerId}")]
-		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BookmarkContainerDto))]
-		[ProducesResponseType(StatusCodes.Status404NotFound)]
-		public IActionResult GetBookmarkContainer(int bookmarkContainerId) {
-			BookmarkContainerDto? container = this.bookmarkContainerService
-				.GetBookmarkContainer(this.GetAuthorizedUserId(), bookmarkContainerId, true, true)
-				?.MapToDto();
-
-			if (container == null)
-				return NotFound();
-
-			return Ok(container);
 		}
 
 		[HttpGet]
@@ -83,37 +51,6 @@ namespace Start.Server.Controllers {
 			return Created(
 				Url.Action(nameof(this.GetBookmark),new { bookmarkId = bookmark.BookmarkId }),
 				bookmark);
-		}
-
-		[HttpPost]
-		[ProducesResponseType(StatusCodes.Status201Created, Type = typeof(BookmarkContainerDto))]
-		[ProducesResponseType(StatusCodes.Status400BadRequest)]
-		public IActionResult CreateBookmarkContainer([FromBody] string title) {
-			BookmarkContainerDto? container = this.bookmarkContainerService
-				.CreateBookmarkContainer(this.GetAuthorizedUserId(), title)
-				?.MapToDto();
-
-			if (container == null)
-				return BadRequest();
-
-			return Created(
-				Url.Action(nameof(this.GetBookmarkContainer),
-					new { bookmarkContainerId = container.BookmarkContainerId }),
-				container);
-		}
-
-		[HttpDelete]
-		[Route("{bookmarkContainerId}")]
-		[ProducesResponseType(StatusCodes.Status200OK)]
-		[ProducesResponseType(StatusCodes.Status404NotFound)]
-		public IActionResult DeleteBookmarkContainer(int bookmarkContainerId) {
-			bool res = this.bookmarkContainerService
-				.DeleteBookmarkContainer(this.GetAuthorizedUserId(), bookmarkContainerId);
-
-			if (!res)
-				return NotFound();
-
-			return Ok();
 		}
 	}
 }
